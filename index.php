1a4d105c30c13e08ed3793558a6817cf617d4b60
mrqwest <mrqwest@gmail.com>
2015-04-28 20:54:05 +0100

    updated index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Sowing the Seed." />
+    <meta http-equiv="cleartype" content="on">
+    <meta name="HandheldFriendly" content="True">
+
+    <title>Seed</title>
+
+    <link rel="shortcut icon" type="image/ico" href="/favicon.ico" />
+
+    <link href="assets/css/app.min.css" rel="stylesheet" />
+</head>
+<body class="">
+    <header role="banner" class="header">
+    </header>
+
+    <nav role="navigation" class="nav">
+
+    </nav>
+
+    <main id="content" role="main" class="main-content">
+    </main>
+
+    <footer role="contentinfo" class="footer">
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.js"><\/script>')</script>
+    <script src="vendor/jquery-2.1.3.min.js"></script>
+    <script src="main.min.js"></script>
+</body>
+</html>
